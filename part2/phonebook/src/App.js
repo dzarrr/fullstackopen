0173e18893bdd3282f1af54c2ffcab7fa32d50db
Etc - Dzar <etc-dzar@Dzars-MacBook-Pro.local>
2020-05-26 08:09:19 +0700

    add solution for exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
+  const [ search, setSearch ] = useState('')
 
   const duplicateName = (newName) => {
     let duplicate = false
@@ -25,7 +27,8 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = {
-        name: newName
+        name: newName,
+        phone: newPhone
       }
       setPersons(persons.concat(newPerson))
     }
@@ -35,12 +38,28 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+ 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with 
+        <input onChange={handleSearchChange}/>
+      </div>
+      <h2>add a new </h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input onChange={handlePhoneChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -48,7 +67,16 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name} style={{margin: 0}}>{person.name}</p>)}
+        {persons
+          .filter(person =>
+              person.name.toLowerCase().includes(search.toLowerCase())
+            )
+          .map(person => 
+              <p key={person.name} style={{margin: 0}}>
+                {person.name} {person.phone}
+              </p>
+            )
+        }
       </div>
     </div>
   )
